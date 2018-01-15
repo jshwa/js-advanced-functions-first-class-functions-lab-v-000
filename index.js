95f4a97e1b1fcc517488585ec394ea00476bdcc3
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-15 21:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-  for (i = 0; i < 2; i++) {
-    return const new_drivers.push(drivers[i])
-  }
+  return drivers.slice(0,2)
 }
